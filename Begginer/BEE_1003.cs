22bf71ccec3e5919d2543ca730ee75f7e61d6a22
YuriC03lho <yuricoelhovieira@gmail.com>
2024-07-28 19:23:28 -0300

    BEE_1003
    Continuando a Diaria.

---
+++ Begginer/BEE_1003.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double A, B, S;
+Console.WriteLine("Você esta executando a Soma!");
+Console.WriteLine("Digite o Valor para A: ");
+A = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o Valor para B");
+B = Double.Parse(Console.ReadLine());
+S = A + B;
+Console.WriteLine("A Soma de A + B é igual á: " + S);
+
+
