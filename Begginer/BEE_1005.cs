b173abb2f5405010e0ddf043f001c5a9bcc1dc48
YuriC03lho <yuricoelhovieira@gmail.com>
2024-07-30 21:26:18 -0300

    Calcular a Media - BEE_1005To1006

---
+++ Begginer/BEE_1005.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+double A, B, Media;
+Console.WriteLine("Digite o primeiro nota para Media do aluno: ");
+A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+Console.WriteLine("Digite a Segunda nota para Media do aluno");
+B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Media = ((A * 3.5) + (B * 7.5)) / 11;
+Console.WriteLine("A Media do aluno é: " + Media.ToString("F5"),CultureInfo.InvariantCulture);
