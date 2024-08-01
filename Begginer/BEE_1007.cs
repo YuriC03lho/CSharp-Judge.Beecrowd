cca62bd68c98de2240e3eff6b9f8a9c362389896
YuriC03lho <yuricoelhovieira@gmail.com>
2024-07-31 21:11:42 -0300

    Diferença/Salario

---
+++ Begginer/BEE_1007.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class BEE_1007
+{
+    static void Main(string[] args)
+    {
+        int A, B, C, D, DIF;
+
+        Console.WriteLine("Digite o Primeiro Numero: ");
+        A = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o Segundo Numero: ");
+        B = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o Terceiro Numero: ");
+        C = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o Quarto e ultimo Numero: ");
+        D = int.Parse(Console.ReadLine());
+
+        DIF = (A * B - C * D);
+        Console.WriteLine("A Diferença entre os Valores é: " + DIF);
+    }
+}
+
---
+++ Begginer/BEE_1008.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+
+class BEE_1008
+{
+    static void Main(string[] args)
+    {
+        //Poderia por o Worktimer para INT más fui ver depois a pergunta rsrs. Depois eu convertia :V
+        int number ;
+        double salary, timePerSalary, worktimer;
+
+        Console.WriteLine("Digite seu Numero de ID caro Caramada: ");
+        number = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Caro Camarada, Quantas horas serviu ao nossa Mãe Russia: ");
+        worktimer = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        Console.WriteLine("Quantas Horas por Salario você é merecido, camarada: ");
+        timePerSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        salary = timePerSalary * worktimer;
+
+        Console.WriteLine("Caro Camarada, obrigado pelos seus Serviços!, Seu ID é: ");
+        Console.WriteLine("Seu Salario é: R$ " + salary.ToString("F2"), CultureInfo.InvariantCulture);
+
+        //Ta Tudo Fluindo como Magica aqui, ta estranho más estou gostando.
+
+    }
+}
